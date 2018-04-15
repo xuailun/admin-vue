61195f0b78ddb314c134c2730712a1fe33b65cc2
xuailun <1013091936@qq.com>
2018-04-15 19:40:57 +0800

    添加登录和home组件配置路由

---
+++ src/components/home/home.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <p>home component</p>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ src/components/login/login.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <p>登录组件</p>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
+
